8eb445eeb943ab0ce117de04a5afaf7544006de8
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-22 23:03:16 -0300

    style: added animation to form

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Scroll Reveal Animation
+ */
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('#form', {
+	reset: true,
+	origin: 'bottom',
+	distance: '20%',
+	opacity: 0,
+	easing: 'ease-in-out',
+	duration: 1550,
+	delay: 150,
+});
